6fb233f2a9ced23fbf04187a9e89bf841cab163b
mirzabekov00 <prog.eldar@gmail.com>
2021-11-08 21:31:43 +0200

    feat(lab1): finish 7 example

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -3,30 +3,18 @@ using System.Threading;
 
 namespace program
 {
-    public class Data
-    {
-        public static int sharedVar;
-        [ThreadStatic] public static int localVar;
-    }
     class Program
     {
-        static void threadFunc(object i)
-        {
-            Console.WriteLine("Thread {0}: Before changing..  Shared: {1}, local: {2}", i, Data.sharedVar, Data.localVar);
-            Data.sharedVar = (int)i;
-            Data.localVar = (int)i;
-            Console.WriteLine("Thread {0}: After changing..  Shared: {1}, local: {2} ", i, Data.sharedVar, Data.localVar);
-        }
         static void Main()
         {
-            Thread t1 = new Thread(threadFunc);
-            Thread t2 = new Thread(threadFunc);
-
-            Data.sharedVar = 3;
-            Data.localVar = 3;
-
-            t1.Start(1); t2.Start(2);
-            t1.Join(); t2.Join();
+            ThreadLocal<int> localSum = new ThreadLocal<int>(() => 0);
+            Thread t1 = new Thread(() => { for (int i = 0; i < 10; i++) localSum.Value++; Console.WriteLine(localSum.Value); });
+            Thread t2 = new Thread(() => { for (int i = 0; i < 10; i++) localSum.Value--; Console.WriteLine(localSum.Value); });
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            Console.WriteLine(localSum.Value);
         }
 
     }
