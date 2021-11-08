2d06d1cb357fa8fd22ab657c166e9ce667318afe
mirzabekov00 <prog.eldar@gmail.com>
2021-11-08 21:34:43 +0200

    feat(lab1): finish 8 example

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -3,20 +3,40 @@ using System.Threading;
 
 namespace program
 {
+    public class ThreadWork
+    {
+        private string sharedWord;
+        public void Run(string secretWord)
+        {
+            sharedWord = secretWord;
+            Save(secretWord);
+            Thread.Sleep(500);
+            Show();
+        }
+        private void Save(string s)
+        {
+            LocalDataStoreSlot slot =
+                   Thread.GetNamedDataSlot("Secret");
+            Thread.SetData(slot, s);
+        }
+        private void Show()
+        {
+            LocalDataStoreSlot slot = Thread.GetNamedDataSlot("Secret");
+            object secretWord = Thread.GetData(slot);
+            Console.WriteLine("Thread {0}, secret word: {1},  shared word: {2} ", Thread.CurrentThread.ManagedThreadId, secretWord, sharedWord);
+        }
+    }
     class Program
     {
         static void Main()
         {
-            ThreadLocal<int> localSum = new ThreadLocal<int>(() => 0);
-            Thread t1 = new Thread(() => { for (int i = 0; i < 10; i++) localSum.Value++; Console.WriteLine(localSum.Value); });
-            Thread t2 = new Thread(() => { for (int i = 0; i < 10; i++) localSum.Value--; Console.WriteLine(localSum.Value); });
-            t1.Start();
-            t2.Start();
-            t1.Join();
-            t2.Join();
-            Console.WriteLine(localSum.Value);
+            ThreadWork thr = new ThreadWork();
+            new Thread(() => thr.Run("one")).Start();
+            new Thread(() => thr.Run("two")).Start();
+            new Thread(() => thr.Run("three")).Start();
+            Thread.Sleep(1000);
         }
-
     }
+
 
 }
