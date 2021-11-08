afdce86cb08e1194c9d9335f50875f932be35d80
mirzabekov00 <prog.eldar@gmail.com>
2021-11-08 21:25:27 +0200

    feat(lab1): finish 5 example

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -3,26 +3,40 @@ using System.Threading;
 
 namespace program
 {
-    class Program
+    class PriorityTesting
     {
-        static void ThreadFunc(object o)
+        static long[] counts;
+        static bool finish;
+        static void ThreadFunc(object iThread)
         {
-            for (int i = 0; i < 20; i++)
-                Console.Write(o);
+            while (true)
+            {
+                if (finish)
+                    break;
+                counts[(int)iThread]++;
+            }
         }
         static void Main()
         {
-            Thread[] t = new Thread[4];
-            for (int i = 0; i < 4; i++)
+            counts = new long[5];
+            Thread[] t = new Thread[5];
+            for (int i = 0; i < t.Length; i++)
+            {
                 t[i] = new Thread(ThreadFunc);
+                t[i].Priority = (ThreadPriority)i;
+            }
+            for (int i = 0; i < t.Length; i++)
+                t[i].Start(i);
 
-            t[0].Start("A"); t[1].Start("B");
-            t[2].Start("C"); t[3].Start("D");
+            Thread.Sleep(10000);
 
-            for (int i = 0; i < 4; i++)
+            finish = true;
+
+            for (int i = 0; i < t.Length; i++)
                 t[i].Join();
+            for (int i = 0; i < t.Length; i++)
+                Console.WriteLine("Thread with priority {0, 15}, Counts: {0} ", (ThreadPriority)i, counts[i]);
         }
-
     }
 
 }
